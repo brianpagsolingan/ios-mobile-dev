168abfbfd243399dab377d9f334ebc7f4d236d12
brian pagsolingan <brian.pagsolingan@gmail.com>
2026-03-31 19:03:25 -0400

    added text field and button

--- lab9/toDoList/toDoList/ViewController.swift
+++ lab9/toDoList/toDoList/ViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBAction func addTask(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
